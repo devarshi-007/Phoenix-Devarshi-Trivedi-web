a5fb4c97ef639703108faf36ef4d55bb9139ac66
devarshi007 <devarshi.trivedi@improwised.com>
2023-04-12 18:44:05 +0530

    BAsic Setup

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+/*Define Pinia store*/
+/*import { useUsersStore } from '~/store/users'
+const UsersStore = useUsersStore()
+console.log(UsersStore);*/
+</script>
+<template>
+  <div class="text-center homepage">Welcome to nuxt3</div>
+</template>
+<style scoped>
+.homepage {
+  margin-top: 400px;
+  font-size: 30px;
+}
+</style>
